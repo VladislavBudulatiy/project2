2ff27ad9acf8bfafcb8193ffe1d303f1660a0be6
Vlad <example@gmail.com>
2022-03-08 20:43:19 +0200

    `second`

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//alert(`Hello`);
+
+
+//const result = confirm(`Are you here`);
+//console.log(result);
+
+const answer = [];
+
+answer[0] = prompt(`Как Ваше имя`, ``);
+answer[1] = prompt(`Как Ваше фамилия`, ``);
+answer[2] = prompt(`сколько вам лет`, ``);
+
+document.write(answer);
